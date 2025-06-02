61b5a069daf9a383bf514aa4541ec010cd8adc27
mertwole <mertwole@gmail.com>
2025-06-02 14:12:47 +0200

    `serialize` package draft

--- bencode/bencode.go
+++ bencode/bencode.go
@@ -4,8 +4,13 @@ import (
 	"io"
 
 	"github.com/mertwole/bittorrent-cli/bencode/deserialize"
+	"github.com/mertwole/bittorrent-cli/bencode/serialize"
 )
 
 func Deserialize(reader io.Reader, value any) error {
 	return deserialize.Deserialize(reader, value)
+}
+
+func Serialize(writer io.Writer, value any) error {
+	return serialize.Serialize(writer, value)
 }
---
+++ bencode/serialize/serialize.go
@@ -0,0 +1,9 @@
+package serialize
+
+import "io"
+
+// TODO
+// NOTE: When encoding dictionaries, keys must be sorted.
+func Serialize(writer io.Writer, value any) error {
+	return nil
+}

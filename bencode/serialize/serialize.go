ef50f20c16276b50b8a9a41446cd6239c1e6c3bc
mertwole <mertwole@gmail.com>
2025-06-05 18:04:21 +0200

    Implement int, string and list serialization

--- bencode/serialize/serialize.go
+++ bencode/serialize/serialize.go
@@ -1,9 +1,45 @@
 package serialize
 
-import "io"
+import (
+	"fmt"
+	"io"
+	"reflect"
+)
 
-// TODO
-// NOTE: When encoding dictionaries, keys must be sorted.
 func Serialize(writer io.Writer, value any) error {
+	valueKind := reflect.TypeOf(value).Kind()
+	valueValue := reflect.ValueOf(value)
+	switch valueKind {
+	case reflect.Pointer, reflect.Interface:
+		deref := valueValue.Elem().Interface()
+		return Serialize(writer, deref)
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		intValue := valueValue.Int()
+
+		fmt.Fprintf(writer, "i%de", intValue)
+	case reflect.String:
+		stringValue := valueValue.String()
+		length := len(stringValue)
+
+		fmt.Fprintf(writer, "%d:%s", length, stringValue)
+	case reflect.Array, reflect.Slice:
+		fmt.Fprint(writer, "l")
+
+		for i := range valueValue.Len() {
+			Serialize(writer, valueValue.Index(i).Interface())
+		}
+
+		fmt.Fprint(writer, "e")
+	case reflect.Struct:
+		fmt.Fprint(writer, "d")
+
+		// TODO
+		// NOTE: When encoding dictionaries, keys must be sorted.
+
+		fmt.Fprint(writer, "e")
+	default:
+		return fmt.Errorf("unserializable type: %v", valueKind)
+	}
+
 	return nil
 }
--- bencode/serialize/serialize_test.go
+++ bencode/serialize/serialize_test.go
@@ -1,1 +1,37 @@
 package serialize
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestIntSerialize(t *testing.T) {
+	testSerialize(int8(10), "i10e", t)
+	testSerialize(int16(-1000), "i-1000e", t)
+	testSerialize(int32(123456789), "i123456789e", t)
+	testSerialize(int64(0), "i0e", t)
+	testSerialize(int(-123456789), "i-123456789e", t)
+}
+
+func TestStringSerialize(t *testing.T) {
+	testSerialize("test", "4:test", t)
+	testSerialize("with the spaces", "15:with the spaces", t)
+}
+
+func TestListSerialize(t *testing.T) {
+	testSerialize([]string{"test"}, "l4:teste", t)
+	testSerialize([]string{"test 1", "test 2"}, "l6:test 16:test 2e", t)
+}
+
+func testSerialize(value any, expected string, t *testing.T) {
+	result := bytes.NewBufferString("")
+
+	err := Serialize(result, value)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if expected != result.String() {
+		t.Errorf("got unexpected value. expected %s, got %s", expected, result.String())
+	}
+}

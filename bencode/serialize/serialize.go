94c0e6cc7c479f2c668405d83c1e4722eeb4bb14
mertwole <33563701+mertwole@users.noreply.github.com>
2025-06-05 21:37:00 +0400

    Implement bencode serializer (#41)

---
+++ bencode/serialize/serialize.go
@@ -0,0 +1,94 @@
+package serialize
+
+import (
+	"fmt"
+	"io"
+	"reflect"
+	"slices"
+)
+
+const fieldTag = "bencode"
+
+func Serialize(writer io.Writer, value any) error {
+	valueKind := reflect.TypeOf(value).Kind()
+	valueValue := reflect.ValueOf(value)
+	switch valueKind {
+	case reflect.Pointer, reflect.Interface:
+		deref := valueValue.Elem()
+		return Serialize(writer, deref.Interface())
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		intValue := valueValue.Int()
+
+		fmt.Fprintf(writer, "i%de", intValue)
+	case reflect.String:
+		stringValue := valueValue.String()
+		length := len(stringValue)
+
+		fmt.Fprintf(writer, "%d:%s", length, stringValue)
+	case reflect.Array, reflect.Slice:
+		fmt.Fprint(writer, "l")
+
+		for i := range valueValue.Len() {
+			err := Serialize(writer, valueValue.Index(i).Interface())
+			if err != nil {
+				return err
+			}
+		}
+
+		fmt.Fprint(writer, "e")
+	case reflect.Struct:
+		fmt.Fprint(writer, "d")
+
+		fields := make(map[string]string, 0)
+		for i := range valueValue.NumField() {
+			fieldType := reflect.TypeOf(value).Field(i)
+			fieldTagValue := fieldType.Tag.Get(fieldTag)
+
+			if fieldTagValue == "" {
+				fieldTagValue = fieldType.Name
+			}
+
+			if _, ok := fields[fieldTagValue]; ok {
+				return fmt.Errorf("fields with duplicate name found: %s", fieldTagValue)
+			}
+
+			fields[fieldTagValue] = fieldType.Name
+		}
+
+		fieldKeys := make([]string, 0)
+		for key := range fields {
+			fieldKeys = append(fieldKeys, key)
+		}
+
+		slices.Sort(fieldKeys)
+
+		for _, fieldKey := range fieldKeys {
+			fieldName := fields[fieldKey]
+
+			field := valueValue.FieldByName(fieldName)
+			fieldInterface := field.Interface()
+
+			fieldInterfaceKind := reflect.TypeOf(fieldInterface).Kind()
+			if fieldInterfaceKind == reflect.Pointer && field.IsNil() {
+				// Optional field.
+				continue
+			}
+
+			err := Serialize(writer, fieldKey)
+			if err != nil {
+				return err
+			}
+
+			err = Serialize(writer, fieldInterface)
+			if err != nil {
+				return err
+			}
+		}
+
+		fmt.Fprint(writer, "e")
+	default:
+		return fmt.Errorf("unserializable type: %v", valueKind)
+	}
+
+	return nil
+}
---
+++ bencode/serialize/serialize_test.go
@@ -0,0 +1,110 @@
+package serialize
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+)
+
+func TestIntSerialize(t *testing.T) {
+	testSerialize(int8(10), "i10e", t)
+	testSerialize(int16(-1000), "i-1000e", t)
+	testSerialize(int32(123456789), "i123456789e", t)
+	testSerialize(int64(0), "i0e", t)
+	testSerialize(int(-123456789), "i-123456789e", t)
+}
+
+func TestStringSerialize(t *testing.T) {
+	testSerialize("test", "4:test", t)
+	testSerialize("with the spaces", "15:with the spaces", t)
+}
+
+func TestListSerialize(t *testing.T) {
+	testSerialize([]string{"test"}, "l4:teste", t)
+	testSerialize([]string{"test 1", "test 2"}, "l6:test 16:test 2e", t)
+}
+
+func TestDictionarySerialize(t *testing.T) {
+	value := dictionaryStruct{
+		IntField:    10,
+		StringField: "test",
+		DictField:   dictionaryStructInner{IntField: 20},
+	}
+
+	expectedString := removeWhitespaces(`
+		d
+			9:DictField
+				d
+					8:IntField
+						i20e
+				e
+			9:int_field
+				i10e
+			12:string@field
+				4:test
+		e
+	`)
+	expectedString = strings.ReplaceAll(expectedString, "@", " ")
+
+	testSerialize(value, expectedString, t)
+}
+
+func TestDictionarySerializeWithOptionalField(t *testing.T) {
+	value := dictionaryStructWithOptionalField{
+		OptionalField: &dictionaryStructInner{IntField: 10},
+	}
+	expected := removeWhitespaces(`
+		d
+			13:OptionalField
+				d
+					8:IntField
+						i10e
+				e
+		e
+	`)
+
+	testSerialize(value, expected, t)
+
+	value = dictionaryStructWithOptionalField{
+		OptionalField: nil,
+	}
+	expected = "de"
+
+	testSerialize(value, expected, t)
+}
+
+func removeWhitespaces(input string) string {
+	input = strings.ReplaceAll(input, " ", "")
+	input = strings.ReplaceAll(input, "\n", "")
+	input = strings.ReplaceAll(input, "\r", "")
+	input = strings.ReplaceAll(input, "\t", "")
+
+	return input
+}
+
+type dictionaryStruct struct {
+	IntField    int    `bencode:"int_field"`
+	StringField string `bencode:"string field"`
+	DictField   dictionaryStructInner
+}
+
+type dictionaryStructWithOptionalField struct {
+	OptionalField *dictionaryStructInner
+}
+
+type dictionaryStructInner struct {
+	IntField int
+}
+
+func testSerialize(value any, expected string, t *testing.T) {
+	result := bytes.NewBufferString("")
+
+	err := Serialize(result, value)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if expected != result.String() {
+		t.Errorf("got unexpected value. expected %s, got %s", expected, result.String())
+	}
+}
